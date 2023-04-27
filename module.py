33e5893f90cb914daeb953c38f4aa93943561546
SARAH MERZOUK <sarahmerzouk10@gmail.com>
2023-04-27 13:39:21 -0400

    changement pour mes classes. Je veux juste une classe qui va spécifier quel est le type de la mesure(distance ou angle)

--- module.py
+++ module.py
@@ -1,10 +1,11 @@
-class Distance ():
-    "L'objet distance comprend les informations sur une distance à prise à par le capteur de distance."
+class Mesure ():
+    "L'objet Mesure comprend les informations sur une mesure prise à partir d'un capteur de mesure(capteur de distance ou servomoteur)."
     
-    def __init__(self, dateHeureMesure, description, dataMesure):
+    def __init__(self, dateHeureMesure, description, dataMesure, type):
         self.dateHeureMesure = dateHeureMesure
         self.description = description
         self.dataMesure = dataMesure
+        self.type = type
         
     def __repr__(self):
         return f'"{self.dateHeureMesure}","{self.description}"'
@@ -12,24 +13,7 @@ class Distance ():
     def afficherMesure(self):
         affichage_date = "Date et l'heure de mesure: " + self.dateHeureMesure + "\r\n"
         affichage_description = "Description: " + self.description + "\r\n"
-        affichage_dataMesure = "Données de mesure: " + self.dataMesure
+        affichage_dataMesure = "Données de mesure: " + self.dataMesure + "\r\n"
+        affichage_type = "Type de la mesure: " + self.type # peut-être «distance» ou «angle»
         
-        return affichage_date + affichage_description + affichage_dataMesure
-    
-class Angle():
-    "L'objet distance comprend les informations sur un angle pris par le servomoteur."
-    
-    def __init__(self, dateHeureMesure, description, dataMesure):
-        self.dateHeureMesure = dateHeureMesure
-        self.description = description
-        self.dataMesure = dataMesure
-        
-    def __repr__(self):
-        return f'"{self.dateHeureMesure}","{self.description}"'
-    
-    def afficherMesure(self):
-        affichage_date = "Date et l'heure de mesure: " + self.dateHeureMesure + "\r\n"
-        affichage_description = "Description: " + self.description + "\r\n"
-        affichage_dataMesure = "Données de mesure: " + self.dataMesure
-        
-        return affichage_date + affichage_description + affichage_dataMesure
+        return affichage_date + affichage_description + affichage_dataMesure + affichage_type
